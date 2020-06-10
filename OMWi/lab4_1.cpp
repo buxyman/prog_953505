ce0bced9e93b3d2600e3915a3ce577ca309a0872
KrasovskiiVladislav <62138349+OMWi@users.noreply.github.com>
2020-06-10 03:22:45 +0300

    [Красовский Владислав] lab#4 variant-2 (#124)
    * lab3
    
    * lab4

---
+++ OMWi/lab4_1.cpp
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+int StrLen(char* str) {
+	int size = 0;
+	while (str[size] != '\0') size++;
+	return size;
+}
+
+void PrintStr(char* str) {
+	for (int i = 0; i < StrLen(str); i++) {
+		printf("%c", str[i]);
+	}
+}
+
+bool IsLower(char ch) {
+	if (ch < 'a' || ch > 'z') return false;
+	return true;
+}
+
+char ToUpper(char ch) {
+	if (IsLower(ch)) {
+		ch += 'A' - 'a';
+	}
+	return ch;
+}
+
+char* Correct(char* str1)
+{
+	char* str2 = (char*)malloc(sizeof(char) * 300);
+	int index2 = 0;
+	for (int index1 = 0; index1 < StrLen(str1); index1++, index2++) {
+		str2[index2] = str1[index1];
+		if ((str1[index1] == ',' || str1[index1] == ':' || str1[index1] == ';' 
+			|| str1[index1] == '.' || str1[index1] == '!' || str1[index1] == '?')
+			&& str1[index1 + 1] != ' ')
+		{
+			if (index1 + 1 == StrLen(str1)) continue;
+			str2[++index2] = ' ';			
+		}
+		if ((str1[index1] == '.' || str1[index1] == '!' || str1[index1] == '?') && IsLower(str1[index1 + 2])) {
+			str2[++index2] = ToUpper(str1[++index1]);
+		}
+	}
+	str2[index2] = '\0';
+	return str2;
+}
+
+int main() {
+	char* str = (char*)malloc(sizeof(char)*200);
+	gets_s(str, 200);
+	printf("Start string:");
+	PrintStr(str);
+	PrintStr("\n");
+	char* temp = str;
+	str = Correct(str);
+	free(temp);
+	printf("Formatted string:");
+	PrintStr(str);
+	free(str);
+}
---
+++ OMWi/lab4_2.cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+int StrLen(char* str) {
+	int size = 0;
+	while (str[size] != '\0') size++;
+	return size;
+}
+
+void PrintStr(char* str) {
+	for (int i = 0; i < StrLen(str); i++) {
+		printf("%c", str[i]);
+	}
+}
+
+int Find(char* str, char ch) {
+	for (int index = 0; index < StrLen(str); index++) {
+		if (str[index] == ch) return index;
+	}
+	return -1;
+}
+
+int FindLast(char* str, char ch) {
+	for (int index = StrLen(str) - 1; index >= 0; index--) {
+		if (str[index] == ch) return index;
+	}
+	return -1;
+}
+
+bool CheckEquivalence(char* str) {
+	int count = 0;
+	for (int index = 0; index < StrLen(str); index++) {
+		if (str[index] == '(') count++;
+		if (str[index] == ')') count--;
+	}
+	if (!count && (Find(str, ')') > FindLast(str, '('))) return true;
+	return false;
+}
+
+char* ReturnBrackets(char* str, int noChangeIndex1, int noChangeIndex2) {
+	str[noChangeIndex1] = '(';
+	str[noChangeIndex2] = ')';
+	return str;
+}
+
+char* Correct(char* str) {
+	if (!CheckEquivalence(str)) return str;
+	int noChangeIndex1 = FindLast(str, '(');
+	int noChangeIndex2 = Find(str, ')');
+	str[noChangeIndex1] = 's';
+	str[noChangeIndex2] = 's';
+	if (FindLast(str, '(') == -1) return ReturnBrackets(str, noChangeIndex1, noChangeIndex2);
+	str[FindLast(str, '(')] = '[';
+	str[Find(str, ')')] = ']';
+	while (FindLast(str, '(') != -1) {
+		str[FindLast(str, '(')] = '{';
+		str[Find(str, ')')] = '}';
+	}
+	return ReturnBrackets(str, noChangeIndex1, noChangeIndex2);
+}
+
+char* GetTextF(char* name) {
+	FILE* fp;
+	fopen_s(&fp, name, "r");
+	if (!fp) return NULL;
+	int size = 0;
+	while (fgetc(fp) != EOF) {
+		size++;
+	}
+	char* text = (char*)malloc(sizeof(char) * (size + 1));
+	fseek(fp, 0, 0);
+	fgets(text, size + 1, fp);
+	return text;
+}
+
+int main() {
+	char* str;
+	char name[] = "data.txt";
+	str = GetTextF(name);
+	PrintStr(str);
+	PrintStr("\n");
+	str = Correct(str);
+	PrintStr(str);
+	free(str);
+}
